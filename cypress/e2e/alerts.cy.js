63726185501b996c00e2faea782ea971bb8fad6a
salmanahmad16 <sulmanmansha@gmail.com>
2024-08-26 18:13:13 +0500

    handle all alerts types

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -14,21 +14,37 @@ describe("Handle alerts", () => {
     })
 
     it("JS confrim alert with OK", () => {
-        cy.get('button[onclick="jsConfirm()"]').should("be.visible").click()
+        cy.get('button[onclick="jsConfirm()"]').should("be.visible").click();
         cy.on("window:confirm", (ele) => {
-            expect(ele).contains("I am a JS Confirm")
+            expect(ele).contains("I am a JS Confirm");
         })
         cy.get('p[id="result"]').should("have.text", "You clicked: Ok")
     })
 
     it("JS confrim alert with Cancel", () => {
-        cy.get('button[onclick="jsConfirm()"]').should("be.visible").click()
+        cy.get('button[onclick="jsConfirm()"]').should("be.visible").click();
         cy.on("window:confirm", (ele) => {
-            expect(ele).contains("I am a JS Confirm")
+            expect(ele).contains("I am a JS Confirm");
         })
         cy.on("window:confirm", () => false)
         cy.get('p[id="result"]').should("have.text", "You clicked: Cancel")
     })
 
+    it("JS alert with prompt value with added some value OK", () => {
+
+        cy.window().then((win)=>{
+            cy.stub(win, "prompt").returns("Welcome");
+        })  
+        cy.get('button[onclick="jsPrompt()"]').should("be.visible").click();
+        cy.get('p[id="result"]').should("have.text", "You entered: Welcome")
+        
+    })
+
+    it.only("JS alert with prompt without value, Cancel", () => {
+
+        cy.get('button[onclick="jsPrompt()"]').should("be.visible").click();
+        cy.get('p[id="result"]').should("have.text", "You entered: null")
+        
+    })
 
 })
