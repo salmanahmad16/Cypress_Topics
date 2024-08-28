c576b0ec3af6cbdf555abedd1711cec8d8f6b1c9
salmanahmad16 <sulmanmansha@gmail.com>
2024-08-28 11:07:56 +0500

    updated with new HTML reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  
   e2e: {
-   
-    defaultCommandTimeout: 10000, // 10 seconds
-    requestTimeout: 15000,
-    pageLoadTimeout: 120000, 
-    reporter: 'mochawesome',
-    reporterOptions:{
-      "chart":true,
-      "overwrite":false,
-      "html":false,
-      "json":true,
-      "reportDir":"cypress/report"
-
-    },
-    },
     setupNodeEvents(on, config) {
-      screenshotOnRunFailure=true;
       require('cypress-mochawesome-reporter/plugin')(on);
- 
     },
-},
-);
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: false,
+    },
+  },
+});
